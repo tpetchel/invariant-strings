4a56fa0e4157b23fa0eace90acd813eabf784c99
Thomas Petchel <tpetchel@gmail.com>
2020-07-01 21:47:02 -0700

    Initial

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+
+namespace invariant_strings
+{
+    class Program
+    { 
+        static void Main(string[] args)
+        {
+            // Lake Sammamish Park
+            double currentLat = 47.555698;
+            double currentLon = -122.065996;
+            // Marymoor Park
+            double destinationLat = 47.663747;
+            double destinationLon = -122.120879;
+
+            int longest = 0;
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                longest = Math.Max(longest, ci.Name.Length);
+            }
+
+            foreach (CultureInfo ci in CultureInfo.GetCultures(CultureTypes.AllCultures))
+            {
+                CultureInfo.CurrentCulture = CultureInfo.ReadOnly(ci);
+                string s = FormattableString.Invariant($"{currentLat},{currentLon}:{destinationLat},{destinationLon}");
+                Console.WriteLine($"{ci.Name}:{new string(' ', (longest-ci.Name.Length+2))}{s}");
+            }
+        }
+    }
+}
